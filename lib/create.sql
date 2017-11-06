623b7e5a9f39b22d09d42bb09fa0dabf4b3517fc
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-06 14:37:09 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,9 +1,9 @@
 CREATE TABLE bears(
-id INTEGER PRIMARY KEY,
-name TEXT,
-age INT,
-gender char(1),
-color TEXT,
-temperament TEXT,
-alive BOOLEAN
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INT,
+  gender char(1),
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
 );
