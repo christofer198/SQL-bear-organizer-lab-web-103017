96605faa46420f4cf3b9c6544a726b8faceb1e62
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-06 14:45:01 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO bear (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 30 , "M", NULL, NULL, NULL);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 30 , "M", NULL, NULL, NULL);
